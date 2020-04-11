9d680986ca04e1585ef07f2270988a2ad0e708b7
eslamelkholy <eslamelkholy5325762@gmail.com>
2020-04-11 16:38:06 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   get 'welcome/index'
+  get 'welcome/hi', to:'welcome#index'
+  resources :articles
+  # The Past Line Equals These 7 Lines
+  # get 'articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new'
+  # post 'articles', to: 'articles#create'
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # put 'articles/:id', to: 'articles#edit'
+  # get 'articles/:id', to: 'articles#show'
+  # delete 'articles/:id', to: 'articles#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
